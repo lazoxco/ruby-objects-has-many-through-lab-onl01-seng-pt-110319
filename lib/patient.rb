99bca42d64ede5f6e72a2537c165dc20c40f003a
Nory Lazo <nlazo@icloud.com>
2019-12-13 08:29:07 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @appointments = []
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_appointment(doctor, date)
+    # appointment = Appointment.new(self, date, doctor)
+    # @appointments << appointment
+    
+    Appointment.new(self, date, doctor)
+  end 
+  
+  def appointments
+    Appointment.all.select { |apointment| appointment.patient == self}
+  end 
+  
+end 
