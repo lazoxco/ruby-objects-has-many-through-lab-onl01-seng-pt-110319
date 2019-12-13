2f60a2ed54547ff12e92157849bf0ae665e9c753
Nory Lazo <nlazo@icloud.com>
2019-12-13 08:48:01 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,8 +4,8 @@ class Appointment
   @@all = []
   
   def initialize(patient, date, doctor)
-    @date = date
     @patient = patient
+    @date = date 
     @doctor = doctor
     @@all << self
   end 
