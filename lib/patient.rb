d7070bdcf8dc009ba9eaef6bdf63fd8cacbc114a
Nory Lazo <nlazo@icloud.com>
2019-12-13 08:54:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,7 +16,7 @@ class Patient
   end 
   
   def appointments
-    self.appointments.collect { |appointment| appointment.doctor }
+    self.appointments.collect { |appointment| appointment.patient }
   end
   
 end 
