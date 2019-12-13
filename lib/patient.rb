0094c14f1d5874d87fe8f11c0196c29adf8e925b
Nory Lazo <nlazo@icloud.com>
2019-12-13 08:34:34 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,7 +16,7 @@ class Patient
   end 
   
   def appointments
-    Appointment.all.select { |appointment| appointment.patient == self }
+    Appointment.all.collecct { |appointment| appointment.patient == self }
   end
   
 end 
